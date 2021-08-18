7241711ee49ec9870ef0c60a1c1c776df1b81223
yuivan <314574808@qq.com>
2021-08-19 00:08:31 +0800

    feat: classnames支持更多属性

--- src/utils/classnames.test.ts
+++ src/utils/classnames.test.ts
@@ -1,12 +1,20 @@
 import { classNames } from "./classnames";
 
-test("classnames", () => {
+test("string", () => {
   expect(classNames("class")).toBe("class");
 });
 
-test("object", () => {
+test("number", () => {
+  expect(classNames(120)).toBe("120");
+});
+
+test("undefined", () => {
+  expect(classNames(undefined)).toBe("");
+});
+
+test("undefined and object", () => {
   expect(
-    classNames("class", {
+    classNames(undefined, {
       pro1: "pro1",
       pro2: undefined,
       pro3: null,
@@ -15,5 +23,5 @@ test("object", () => {
       pro6: {},
       pro7: "",
     })
-  ).toBe("class pro1 pro4 pro6");
+  ).toBe("pro1 pro4 pro6");
 });
--- src/utils/classnames.ts
+++ src/utils/classnames.ts
@@ -1,10 +1,14 @@
 export type Mapping = { [key: string]: unknown };
-export type Argument = string | Mapping;
+export type Value = string | number | boolean | undefined | null;
+export type Argument = Value | Mapping;
 export const classNames = (...arg: Argument[]): string => {
   const classes: string[] = [];
   arg.forEach((item) => {
-    if (typeof item === "string") {
-      classes.push(item);
+    if (item == null) {
+      return;
+    }
+    if (typeof item === "string" || typeof item === "number") {
+      classes.push(String(item));
     } else {
       for (const [key, value] of Object.entries(item)) {
         if (value) {
