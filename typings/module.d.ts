56c2eebdb6e6728d804e05f4e0f271304f82633f
yiyawu <724057561@qq.com>
2021-08-18 21:45:25 +0800

    feat: 集成tailwind，react-grid-layout

--- typings/module.d.ts
+++ typings/module.d.ts
@@ -1,1 +1,10 @@
 declare module "*.svg";
+declare module "less-vars-to-js" {
+  const lessToJS: any;
+  export default lessToJS;
+}
+
+declare module "tailwindcss" {
+  const tailwindcss: any;
+  export default tailwindcss;
+}
