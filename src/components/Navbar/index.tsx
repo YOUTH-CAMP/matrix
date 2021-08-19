41ea1acee5b56c418842ce2ad7c75e0e0b5b09e1
zhangyukicn <yukizhangcn@gmail.com>
2021-08-19 22:34:35 +1000

    upadte header version2

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -2,17 +2,45 @@ import React from "react";
 import zhCN from "antd/lib/locale/zh_CN";
 import { PageHeader, Tabs, Button, Statistic, Descriptions } from "antd";
 import style from "./index.module.less";
-//import HeaderSearch from 'ant-design-pro/lib/HeaderSearch';
+import { AppstoreOutlined } from "@ant-design/icons";
+import { Menu } from "antd";
+
+const { SubMenu } = Menu;
+const rootSubmenuKeys = ["sub1", "sub2", "sub4"];
+const Sider = () => {
+  const [openKeys, setOpenKeys] = React.useState(["sub1"]);
+  const onOpenChange = (keys: any) => {
+    const latestOpenKey = keys.find(
+      (key: string) => openKeys.indexOf(key) === -1
+    );
+    if (rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
+      setOpenKeys(keys);
+    } else {
+      setOpenKeys(latestOpenKey ? [latestOpenKey] : []);
+    }
+  };
+};
 
 function App(): JSX.Element {
   return (
     <div className={style.flexframe}>
       <div className={style.center}>
-        <PageHeader title="Matrix"></PageHeader>
+        <PageHeader title="Matrix" style={{ paddingRight: "0px" }}></PageHeader>
         <div className={style.buttonstyle}>
           <Button>首页</Button>
           <Button>订阅</Button>
           <Button>快讯</Button>
+        </div>
+        <div className={style.menu}>
+          <div>
+            <Menu mode="inline" style={{ paddingLeft: "0px" }}>
+              <SubMenu key="sub1" icon={<AppstoreOutlined />}>
+                <Menu.Item key="1">首页</Menu.Item>
+                <Menu.Item key="2">订阅</Menu.Item>
+                <Menu.Item key="3">快讯</Menu.Item>
+              </SubMenu>
+            </Menu>
+          </div>
         </div>
       </div>
       {/* <HeaderSearch
