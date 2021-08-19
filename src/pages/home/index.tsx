50f782524adff910bc82cd82037b06783d225e34
lida33 <8767208+lida33@user.noreply.gitee.com>
2021-08-19 22:11:40 +0800

    fix:修改资讯卡片Props传入

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,36 @@
 import * as React from "react";
-import NewsCard from "./components/NewsCard";
-const Home = () => {
+import ArticleCard from "./components/ArticleCard";
+import { request } from "../../utils/request";
+
+type ArticleData = {
+  success: boolean;
+  data: Array<ArticleList>;
+};
+
+const Home: React.FC = () => {
+  const [articlesList, setArticlesList] = React.useState<ArticleList[]>([
+    {
+      name: "0",
+      classificationId: 1,
+      articles: [{ id: 0, title: "", isClosed: false }],
+    },
+  ]);
+  const [loaded, setLoaded] = React.useState<boolean>(false);
+  React.useEffect(() => {
+    const fetchData = async () => {
+      const res = (await request("articleList")) as ArticleData;
+      setLoaded(res.success);
+      setArticlesList(res.data);
+    };
+    !loaded && fetchData();
+  }, []);
+
   return (
     <>
-      <NewsCard />
-      <NewsCard />
+      {loaded &&
+        articlesList.map((item, index) => {
+          return <ArticleCard key={"card-" + index} article={item} />;
+        })}
     </>
   );
 };
