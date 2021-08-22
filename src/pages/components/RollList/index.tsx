b25d321d769ab509bb9def63a19966c4e6185081
wangshunping <wsp01110432@antgroup.com>
2021-08-22 20:07:50 +0800

    feat: 暂时隐藏侧边栏

--- src/pages/components/RollList/index.tsx
+++ src/pages/components/RollList/index.tsx
@@ -1,55 +1,74 @@
 import React, { useCallback, useEffect, useRef, useState } from "react";
 import styles from "./index.module.less";
-import Cards from '../../../components/Cards';
+import Cards from "../../../components/Cards";
+import { LocalstorageKey } from "@/constants";
+import { useRequest } from "@/hooks/useRequest";
 
 export function RollList() {
   const [containerHeight, setContainerHeight] = useState(0);
   const [contentHeight, setContentHeight] = useState(0);
+  const { data } = useRequest("getNews");
 
   const containerRef = useCallback((node: any) => {
     if (!node) return;
+    console.log("操A", node.clientHeight);
     setContainerHeight(node.clientHeight);
   }, []);
   const contentRef = useCallback((node: any) => {
     if (!node) return;
+    console.log("操B", node.clientHeight);
     setContentHeight(node.clientHeight);
   }, []);
 
   const [offsetY, setOffsetY] = useState(0);
 
-  const scroll = useCallback(() => {
-    console.log({ offsetY });
-    const nextY = offsetY + 0.1;
-    setOffsetY(nextY);
+  const scroll = (y: number = 0) => {
+    if (localStorage.getItem(LocalstorageKey.isStopScroll) === "true") return;
+    setOffsetY(y + 0.1);
     requestAnimationFrame(() => {
-      scroll();
+      scroll(y > contentHeight - containerHeight ? 0 : y);
     });
-  }, [containerHeight, contentHeight, offsetY, setOffsetY]);
-  function onMouseEnter() {}
+  };
 
-  function onMouseLeave() {}
+  function onMouseEnter() {
+    localStorage.setItem(LocalstorageKey.isStopScroll, "true");
+  }
+
+  function onMouseLeave() {
+    localStorage.setItem(LocalstorageKey.isStopScroll, "false");
+    scroll(offsetY);
+  }
 
   useEffect(() => {
-    if (contentHeight > 0) {
-      scroll();
-    }
-  }, [contentHeight]);
+    // console.log(contentHeight, containerHeight);
+    // if (contentHeight > 0 && containerHeight > 0) {
+    //   console.log("fuck", contentHeight, containerHeight);
+    //   onMouseLeave();
+    // }
+  }, [contentHeight, containerHeight]);
 
   return (
     <div className={styles.container} ref={containerRef}>
       <div
         ref={contentRef}
         className={styles.cardList}
-        onMouseEnter={onMouseEnter}
-        onMouseLeave={onMouseLeave}
+        // onMouseEnter={onMouseEnter}
+        // onMouseLeave={onMouseLeave}
         style={{
           transform: `translate3d(0, -${offsetY}px, 0)`,
         }}
       >
-        {Array.from({ length: 20 }).map((x, i) => {
+        {(data as any)?.data?.map((x: any, i: number) => {
           return (
             <div className={styles.cardContainer} key={i}>
-              {i}布局占位，开发中...
+              <Cards
+                renderData={{
+                  title: x.title,
+                  content: x.content,
+                  imageSrc: x.imageSrc,
+                  link: x.link,
+                }}
+              />
             </div>
           );
         })}
