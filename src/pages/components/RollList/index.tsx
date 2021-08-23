eb3640bafe42d49a849528812557b0a1238eb0d9
zhangyukicn <yukizhangcn@gmail.com>
2021-08-23 15:49:04 +1000

    update right UI framework

--- src/pages/components/RollList/index.tsx
+++ src/pages/components/RollList/index.tsx
@@ -30,7 +30,7 @@ export function RollList() {
     if (localStorage.getItem(LocalstorageKey.isStopScroll) === "true") return;
     setOffsetY(y);
     requestAnimationFrame(() => {
-      scroll((y > contentHeight - containerHeight ? -0.1 : y) + 0.1);
+      scroll((y > contentHeight - containerHeight ? -0.1 : y) + 0.3);
     });
   };
 
@@ -51,6 +51,10 @@ export function RollList() {
 
   return (
     <div className={styles.container} ref={containerRef}>
+      <div className={styles.header}>
+        <h1>快讯为您推荐中...</h1>
+
+      </div>
       <div
         ref={contentRef}
         className={styles.cardList}
